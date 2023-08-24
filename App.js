ceddf7fa725041e3631b9fead8f25d1c8edba26d
Damien <damien.teo.ks@gmail.com>
2023-08-24 11:51:19 +0800

    Basic styling

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+      <Text style={styles.section}>
+        Open up App.js to start working on your app!
+      </Text>
+
+      <Button title="Button" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  section: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "blue",
+    padding: 16,
   },
 });
