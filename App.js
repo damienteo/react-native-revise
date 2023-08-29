251178f87bac3dc183e25ce3f46a8e499cd8dd8a
Damien <damien.teo.ks@gmail.com>
2023-08-29 11:19:47 +0800

    StartGameScreen

--- App.js
+++ App.js
@@ -1,83 +1,11 @@
+import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import {
-  Button,
-  FlatList,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  ScrollView,
-} from "react-native";
+import { StyleSheet } from "react-native";
 
-import GoalItem from "./components/GoalItem";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  const [goal, setGoal] = useState("");
-  const [goals, setGoals] = useState([]);
-
-  const goalInputHandler = (nextText) => {
-    setGoal(nextText);
-  };
-  const addGoal = () => {
-    setGoals((prevState) => [
-      ...prevState,
-      { text: goal, id: Math.random().toString() },
-    ]);
-    setGoal("");
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Goal"
-          style={styles.textInput}
-          onChangeText={goalInputHandler}
-        />
-        <Button title="Add Goal" onPress={addGoal} />
-      </View>
-      <View style={styles.goalsContainer}>
-        <Text>List of Goals</Text>
-        <FlatList
-          data={goals}
-          renderItem={(data) => <GoalItem text={data.item.text} />}
-          keyExtractor={(item, index) => item.id}
-        />
-      </View>
-    </View>
-  );
+  return <StartGameScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16,
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
-  },
-  textInput: {
-    padding: 8,
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
-  },
-  goalsContainer: { flex: 5 },
-  goalItem: {
-    margin: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-    padding: 8,
-  },
-  goalText: {
-    color: "white",
-  },
-});
+const styles = StyleSheet.create({});
---
+++ components/PrimaryButton.js
@@ -0,0 +1,40 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+export default function PrimaryButton({ children, onPress }) {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.pressed, styles.buttonContainer]
+            : styles.buttonContainer
+        }
+        onPress={onPress}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonContainer: {
+    backgroundColor: "#72063c",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
