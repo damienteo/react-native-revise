f45189d994592e6ccab99932528fe70069fa8273
Damien <damien.teo.ks@gmail.com>
2023-09-04 16:46:32 +0800

    Setup for new app

--- App.js
+++ App.js
@@ -1,64 +1,28 @@
 import { useState } from "react";
-import { SafeAreaView, StyleSheet, ImageBackground } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ImageBackground,
+  Text,
+  View,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
 
-import StartGameScreen from "./screens/StartGameScreen";
-import GameScreen from "./screens/GameScreen";
-import GameOverScreen from "./screens/GameOverScreen";
-
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [gameOver, setGameOver] = useState(true);
-
-  const [fontsLoaded] = useFonts({
-    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-  });
-
-  if (!fontsLoaded) return <AppLoading />;
-
-  const handlePickNumber = (number) => {
-    setUserNumber(number);
-    setGameOver(false);
-  };
-
-  const handleGameOver = () => {
-    setGameOver(true);
-  };
-
-  let screen = <StartGameScreen onPickNumber={handlePickNumber} />;
-
-  if (userNumber) {
-    screen = (
-      <GameScreen userNumber={userNumber} handleGameOver={handleGameOver} />
-    );
-  }
-
-  if (gameOver && userNumber) {
-    screen = <GameOverScreen />;
-  }
-
   return (
-    <LinearGradient colors={["#3b021f", "#ddb52f"]} style={styles.rootScreen}>
-      <ImageBackground
-        source={require("./assets/images/dice.png")}
-        resizeMode="cover"
-        style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
-      >
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  rootScreen: {
+  container: {
     flex: 1,
-  },
-  backgroundImage: {
-    opacity: 0.15,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- screens/GameOverScreen.js
+++
@@ -1,9 +0,0 @@
-import { View, StyleSheet, Text } from "react-native";
-
-export default function GameOverScreen() {
-  return (
-    <View>
-      <Text>Game Over</Text>
-    </View>
-  );
-}
